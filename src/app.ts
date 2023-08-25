5803dd665cb058a9a51087b9edbd0d18a70680d7
Ali <alikerimli94@gmail.com>
2023-08-25 15:15:05 +0200

    private access modifier

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string
+  private employees: string[] = []
 
   constructor(name: string) {
     this.name = name
@@ -8,12 +9,29 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name)
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 const acc = new Department('Accounting')
 
-const accCopy = {describe: acc.describe, name: 'DUMMY' }
+acc.addEmployee('Max')
+acc.addEmployee('Manu')
 
-accCopy.describe() // Department: undefined if "this: Department" is not used and name is not defined in accCopy
+// acc.employees[2] = 'Anna' // this is possible if employees is public
+
+acc.describe()
+acc.printEmployeeInformation()
+
+// const accCopy = { describe: acc.describe, name: 'DUMMY' }
+
+// accCopy.describe() // Department: undefined if "this: Department" is not used and name is not defined in accCopy
 
 console.log(acc)
