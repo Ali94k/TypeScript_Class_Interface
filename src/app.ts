9a3510095026a5e66bdc5fad539593aa93e64aa1
Ali <alikerimli94@gmail.com>
2023-08-25 15:20:25 +0200

    short hand prop declaration

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,24 @@
 class Department {
-  name: string
   private employees: string[] = []
 
-  constructor(name: string) {
-    this.name = name
-  }
+  constructor(private readonly id: string, public name: string) {}
 
   describe(this: Department) {
-    console.log('Department: ' + this.name)
+    console.log('Department: ' + this.name + ' ID: ' + this.id)
   }
 
   addEmployee(employee: string) {
+    //this.id = 'd2' // this is not possible if id is readonly
     this.employees.push(employee)
   }
 
-    printEmployeeInformation() {
-        console.log(this.employees.length)
-        console.log(this.employees)
-    }
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
-const acc = new Department('Accounting')
+const acc = new Department('id1', 'Accounting')
 
 acc.addEmployee('Max')
 acc.addEmployee('Manu')
